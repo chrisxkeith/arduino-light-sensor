0711028018cd551ea81daac627c32db69e43622a
CK <chris.keith@gmail.com>
2024-08-19 15:55:44 -0700

    Copy OLED and spinner. Still need to figure out include issues

--- arduino-light-sensor/arduino-light-sensor.ino
+++ arduino-light-sensor/arduino-light-sensor.ino
@@ -13,6 +13,101 @@ class Config {
 };
 Config config;
 
+/*
+// If not defined, assumes QWIIC-cabled OLED.
+// #define USE_OLED_SHIELD
+
+#ifdef USE_OLED_SHIELD
+#include <SparkFunMicroOLED.h>
+#else
+#include <SFE_MicroOLED.h>
+#endif
+#include <math.h>
+
+class OLEDWrapper {
+  public:
+#ifdef USE_OLED_SHIELD
+    MicroOLED* oled = new MicroOLED();
+#else
+    MicroOLED* oled = new MicroOLED(MODE_I2C, 9, 1, CS_DEFAULT);
+#endif
+
+    OLEDWrapper() {
+        oled->begin();    // Initialize the OLED
+        oled->clear(ALL); // Clear the display's internal memory
+        oled->display();  // Display what's in the buffer (splashscreen)
+        delay(1000);     // Delay 1000 ms
+        oled->clear(PAGE); // Clear the buffer.
+    }
+
+    void display(String title, int font, uint8_t x, uint8_t y) {
+        oled->clear(PAGE);
+        oled->setFontType(font);
+        oled->setCursor(x, y);
+        oled->print(title);
+        oled->display();
+    }
+
+    void display(String title, int font) {
+        display(title, font, 0, 0);
+    }
+
+    void invert(bool invert) {
+      oled->invert(invert);
+    }
+
+    void displayNumber(String s) {
+        // To reduce OLED burn-in, shift the digits (if possible) on the odd minutes.
+        int x = 0;
+        if (Time.minute() % 2) {
+            const int MAX_DIGITS = 5;
+            if (s.length() < MAX_DIGITS) {
+                const int FONT_WIDTH = 12;
+                x += FONT_WIDTH * (MAX_DIGITS - s.length());
+            }
+        }
+        display(s, 3, x, 0);
+    }
+    void clear() {
+      oled->clear(ALL);
+    }
+};
+OLEDWrapper oledWrapper;
+
+class Spinner {
+  private:
+    int middleX = oledWrapper.oled->getLCDWidth() / 2;
+    int middleY = oledWrapper.oled->getLCDHeight() / 2;
+    int xEnd, yEnd;
+    int lineWidth = min(middleX, middleY);
+    int color;
+    int deg;
+
+  public:
+    Spinner() {
+      color = WHITE;
+      deg = 0;
+    }
+
+    void display() {
+      xEnd = lineWidth * cos(deg * M_PI / 180.0);
+      yEnd = lineWidth * sin(deg * M_PI / 180.0);
+
+      oledWrapper.oled->line(middleX, middleY, middleX + xEnd, middleY + yEnd, color, NORM);
+      oledWrapper.oled->display();
+      deg++;
+      if (deg >= 360) {
+        deg = 0;
+        if (color == WHITE) {
+          color = BLACK;
+        } else {
+          color = WHITE;
+        }
+      }
+    }
+};
+Spinner spinner;
+*/
 class Sensor {
   private:
     int     pin;
@@ -68,5 +163,6 @@ void setup() {
 void loop() {
   lightSensor1.sample();
   lightSensor1.publish();
+  delay(2000);
 }
  
