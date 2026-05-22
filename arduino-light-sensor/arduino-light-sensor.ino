62f747515b7fdd996980de1e1506d76387b0975a
Chris Keith <chris.keith@gmail.com>
2026-05-21 17:51:52 -0700

    Commit to Giga display:

--- arduino-light-sensor/arduino-light-sensor.ino
+++ arduino-light-sensor/arduino-light-sensor.ino
@@ -25,135 +25,6 @@ class Utils {
 unsigned long Utils::lastPrintln = 0;
 bool Utils::debug = false;
 
-// #define USE_ZIO
-#ifdef USE_ZIO
-#include <U8g2lib.h>
-
-U8G2_SSD1327_EA_W128128_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
-const int COLOR_WHITE = 1;
-const int COLOR_BLACK = 0;
-class OLEDWrapper {
-  private:
-      const int START_BASELINE = 50;
-      int       baseLine = START_BASELINE;
-  public:
-    void u8g2_prepare(void) {
-      u8g2.setFont(u8g2_font_fur49_tn);
-      u8g2.setFontRefHeightExtendedText();
-      u8g2.setDrawColor(COLOR_WHITE);
-      u8g2.setFontDirection(0);
-    }
-    void display(int x, int y, String s) {
-      u8g2_prepare();
-      u8g2.clearBuffer();
-      u8g2.setFont(u8g2_font_fur11_tf);
-      u8g2.drawUTF8(x, y, s.c_str());
-      u8g2.sendBuffer();
-    }
-    void clear() {
-      u8g2_prepare();
-      u8g2.clearBuffer();
-      u8g2.sendBuffer();
-    }
-    void shiftDisplay(int shiftAmount) {
-      this->baseLine += shiftAmount;
-      if (this->baseLine > 70) {
-        this->baseLine = START_BASELINE;
-      }
-    }
-    void startup() {
-      pinMode(10, OUTPUT);
-      pinMode(9, OUTPUT);
-      digitalWrite(10, 0);
-      digitalWrite(9, 0);
-      if (!u8g2.begin()) {
-        Utils::publish("u8g2.begin() failed!");
-      } else {
-        Utils::publish("u8g2.begin() succeeded!");
-        u8g2.setBusClock(400000);
-      }
-    }
-    void startDisplay(const uint8_t *font) {
-      u8g2_prepare();
-      u8g2.clearBuffer();
-      u8g2.setFont(font);
-    }
-    void display(String s, uint8_t x, uint8_t y) {
-      u8g2.setCursor(x, y);
-      u8g2.print(s.c_str());
-    }
-    void endDisplay() {
-      u8g2.sendBuffer();
-    }
-    void display(String s) {
-      startDisplay(u8g2_font_fur11_tf);
-      display(s, 0, 16);
-      endDisplay();
-    }
-    void display(String s[], int nStrings) {
-      startDisplay(u8g2_font_fur11_tf);
-      for (int i = 0; i < nStrings; i++) {
-        display(s[i], 0, 16 + (i * 16));
-      }
-      endDisplay();
-    }
-    void setDrawColor(int color) {
-      u8g2.setDrawColor(color);
-    }
-    void drawLine(int x0, int y0, int x1, int y1) {
-      u8g2.drawLine(x0, y0, x1, y1);
-    }
-    void test1() {
-      for (u8g2_uint_t h = u8g2.getHeight(); h > 95; h -= 1) {
-        startDisplay(u8g2_font_fur11_tf);
-        u8g2.drawFrame(0, 0, getWidth(), h);
-        u8g2.drawUTF8(8, 32, String(h).c_str());
-        endDisplay();
-      }
-    }
-    void test2() {
-      for (int h = 8; h > -8; h -= 1) {
-        startDisplay(u8g2_font_fur11_tf);
-        u8g2.drawLine(0, h, getWidth(), h);
-        u8g2.drawUTF8(8, 32, String(h).c_str());
-        endDisplay();
-      }
-    }
-    void test3() {
-      unsigned long start = millis();
-      startDisplay(u8g2_font_fur11_tf);
-      for (int y = 0; y < getHeight(); y++) {
-        u8g2.drawPixel(0, y);
-        endDisplay();
-      }
-      unsigned long duration = millis() - start;
-      Utils::publish("test3() duration: " + String(duration) + " ms");
-    }
-    void test4() {
-      unsigned long start = millis();
-      for (int y = 0; y < getHeight(); y++) {
-        startDisplay(u8g2_font_fur11_tf);
-        u8g2.drawPixel(0, y);
-        endDisplay();
-      }
-      unsigned long duration = millis() - start;
-      Utils::publish("test4() duration: " + String(duration) + " ms");
-    }
-    void test() {
-      test3();
-      test4();
-/*  test3() duration: 36840 ms
-    test4() duration: 36868 ms
-*/
-    }
-    int getHeight() {
-      return 96; // ??? why does u8g2.getHeight() return 128 ???
-    }
-    int getWidth() {
-      return u8g2.getWidth();
-    }
-};
-#else
 #include "Arduino_GigaDisplay_GFX.h"
 const int COLOR_WHITE = 0x65535;
 const int COLOR_BLACK = 0x0;
@@ -163,48 +34,44 @@ GigaDisplay_GFX display_;
 class OLEDWrapper {
   private:
     uint16_t currentColor = COLOR_WHITE;
+    const int DEFAULT_FONT_SIZE = 3;
   public:
-    void u8g2_prepare(void) {
-    }
-    void display(int x, int y, String s) {
-    }
     void clear() {
       display_.fillScreen(COLOR_BLACK);
     }
     void startup() {
-      delay(3000);
+      delay(1000);
       display_.begin(); //init library
+      clear();
+      display_.setRotation(1);
     }
-    void startDisplay(const uint8_t *font) {
-    }
-    void display(String s, uint8_t x, uint8_t y) {
-    }
-    void endDisplay() {
+    void display(String s, int textSize, uint8_t x, uint8_t y) {
+      display_.setCursor(x,y); //x,y
+      display_.setTextSize(textSize); //adjust text size
+      display_.print(s); //print
     }
     void display(String s) {
+      display(s, DEFAULT_FONT_SIZE, 10, 10);
     }
     void display(String s[], int nStrings) {
+      for (int i = 0; i < nStrings; i++) {
+        display(s[i], DEFAULT_FONT_SIZE, 10, 32 + (i * 32));
+      }
     }
     void setDrawColor(int color) {
       currentColor = color;
     }
     void drawLine(int x0, int y0, int x1, int y1) {
-      display_.drawLine(x0, y0, x1, y1, currentColor);
-    }
-    void test1() {
-      display_.setCursor(10,10); //x,y
-      display_.setTextSize(5); //adjust text size
-      display_.print("Hello World!"); //print
+      // Rotate not happening automatically?
+      display_.drawLine(y0, x0, y1, x1, currentColor);
     }
     int getHeight() {
-      return 800;
+      return display_.height();
     }
     int getWidth() {
-      return 480;
+      return display_.width();
     }
 };
-#endif
-
 OLEDWrapper* oledWrapper = nullptr;
 
 class Spinner {
@@ -233,10 +100,8 @@ class Spinner {
       int xEnd = lineLength * cos(deg * M_PI / 180.0);
       int yEnd = lineLength * sin(deg * M_PI / 180.0);
 
-      oledWrapper->startDisplay(0);
       oledWrapper->setDrawColor(color);
       oledWrapper->drawLine(middleX, middleY, middleX + xEnd, middleY + yEnd);
-      oledWrapper->endDisplay();
       deg += incrementDegrees;
       if (deg >= 360) {
         deg = 0;
@@ -329,7 +194,7 @@ Sensor lightSensor1(A0, "Arduino light sensor");
 
 class Config {
   public:
-    const String build = "Sat Apr 25 01:10:52 PM PDT 2026";
+    const String build = "Thu May 21 05:43:26 PM PDT 2026";
     void dump() {
       String s("gitHubRepository: https://github.com/chrisxkeith/arduino-light-sensor");
       Utils::publish(s);
@@ -362,7 +227,7 @@ class App {
       for (int i = 0; i < totalSeconds; i++) {
         lightSensor1.sample();
         int value = lightSensor1.getValue();
-        oledWrapper->display(String(totalSeconds - i), 0, 1);
+        oledWrapper->display(String(totalSeconds - i));
         Utils::publish(String(value)); 
         delay(1000);
         total += value;
@@ -372,7 +237,7 @@ class App {
       String avgStr("Average: ");
       avgStr.concat(avg);
       Utils::publish(avgStr);
-      oledWrapper->display(String(avg), 0, 1);
+      oledWrapper->display(String(avg));
       delay(5000);
     }
     void display_on_oled() {
@@ -417,15 +282,10 @@ class App {
     }
     void showBuild() {
       oledWrapper->clear();
-      String s[2] = { config.build, config.build.substring(27, 31) };
+      String s[2] = { String("Build:"), config.build };
       oledWrapper->display(s, 2);
       delay(3000);
       oledWrapper->clear();
-    } 
-    void test() {
-      oledWrapper->test1();
-      Utils::publish("After test(), waiting for '.'");
-      while (Utils::waitForSerial(".")) {}
     } 
     void testSpinner() {
       lightSensor1.testing = true;
@@ -452,8 +312,6 @@ void Utils::checkSerial() {
       lightSensor1.publish = true;
     } else if (command.equals("publish off")) {
       lightSensor1.publish = false;
-    } else if (command.equals("test")) {
-      app.test();
     } else if (command.equals("showBuild")) {
       app.showBuild();
     } else if (command.equals("testSpinner")) {
